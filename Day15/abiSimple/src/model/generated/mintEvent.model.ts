56a1b2fbeaf03f87b01357b45a691c9df81d5bcb
FrankBevr <frank_dierolf@web.de>
2023-02-10 22:03:36 +0100

    :construction: (./abiSimple): add abiSimple
    play around

---
+++ Day15/abiSimple/src/model/generated/mintEvent.model.ts
@@ -0,0 +1,33 @@
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, ManyToOne as ManyToOne_, Index as Index_} from "typeorm"
+import * as marshal from "./marshal"
+import {Block} from "./block.model"
+import {Transaction} from "./transaction.model"
+
+@Entity_()
+export class MintEvent {
+    constructor(props?: Partial<MintEvent>) {
+        Object.assign(this, props)
+    }
+
+    @PrimaryColumn_()
+    id!: string
+
+    @Index_()
+    @ManyToOne_(() => Block, {nullable: true})
+    block!: Block
+
+    @Index_()
+    @ManyToOne_(() => Transaction, {nullable: true})
+    transaction!: Transaction
+
+    @Index_()
+    @Column_("text", {nullable: false})
+    name!: string
+
+    @Index_()
+    @Column_("text", {nullable: false})
+    to!: string
+
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    amount!: bigint
+}
