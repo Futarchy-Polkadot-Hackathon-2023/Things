f1b019c2ea6948d366b0c24a494ee3d4b86486cf
FrankBevr <frank_dierolf@web.de>
2023-02-11 09:55:24 +0100

    :construction: (./Day16/evm-tutorial): add evm-tutorial
    sqd init evm-tutorial --template evm, try my best to get a understanding.

---
+++ Day16/evm-tutorial/src/contract.ts
@@ -0,0 +1,7 @@
+import {Store} from '@subsquid/typeorm-store'
+import {Contract} from './model'
+
+export const contractAddress = "0xac5c7493036de60e63eb81c5e9a440b42f47ebf5"
+
+let contractEntity: Contract | undefined;
+
