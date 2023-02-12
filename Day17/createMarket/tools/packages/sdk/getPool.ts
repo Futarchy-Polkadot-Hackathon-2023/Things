26c1e022d4231668634144451ab2075c220d1d4e
FrankBevr <frank_dierolf@web.de>
2023-02-12 23:03:10 +0100

    :construction: (./Day17/createMarket): try local node and failed ._.

--- Day17/createMarket/tools/packages/sdk/getPool.ts
+++ Day17/createMarket/tools/packages/sdk/getPool.ts
@@ -3,7 +3,7 @@ import SDK, {util} from "@zeitgeistpm/sdk"
 async function main(){
   // const ZTGNET = "wss://bsr.zeigeist.pm"
   // const ZTGNET = "wss://bp-rpc.zeitgeist.pm"
-  // const ZTGNET = "wss://"
+  const ZTGNET = "wss://127.0.0.1:9944"
   const sdk = await SDK.initialize(ZTGNET)
   const marketId = 8
   const filter = ["question","tags"]
