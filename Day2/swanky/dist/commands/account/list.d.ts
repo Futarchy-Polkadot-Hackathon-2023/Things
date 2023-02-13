fa0f61a6104f5e7d850a14cfd43bb070f1e1ee33
FrankBevr <frank_dierolf@web.de>
2023-02-13 06:54:01 +0100

    :recycle: (./): mv Day 1-5 to Notes_and_trials

--- Day2/swanky/dist/commands/account/list.d.ts
+++
@@ -1,6 +0,0 @@
-import { Command } from "@oclif/core";
-export declare class CreateAccount extends Command {
-    static description: string;
-    static aliases: string[];
-    run(): Promise<void>;
-}
--- Day2/swanky/dist/lib/prompts.d.ts
+++
@@ -1,9 +0,0 @@
-import { Answers, ListQuestion, Question } from "inquirer";
-export declare function pickTemplate(templateList: {
-    message: string;
-    value: string;
-}[]): ListQuestion<Answers>;
-export declare function name(subject: string, initial?: (answers: Answers) => string, questionText?: string): Question<Answers>;
-export declare function email(initial?: string, questionText?: string): Question<Answers>;
-export declare function choice(subject: string, questionText: string): Question<Answers>;
-export declare function pickLanguage(): ListQuestion<Answers>;
